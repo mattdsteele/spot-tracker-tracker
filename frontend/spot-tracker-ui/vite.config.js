69f1d61826dd7d71d41028ab86208f2d16cb8e6f
mattdsteele <orphum@gmail.com>
2022-08-19 02:25:03 -0500

    Add GA

--- frontend/spot-tracker-ui/vite.config.js
+++ frontend/spot-tracker-ui/vite.config.js
@@ -1,8 +1,18 @@
-import {visualizer} from 'rollup-plugin-visualizer';
+import { visualizer } from 'rollup-plugin-visualizer';
+import path from 'path';
+import { partytownVite } from '@builder.io/partytown/utils';
 
 /** @type {import('vite').UserConfig} */
 export default {
-  plugins: [visualizer({
-    filename: 'target/stats.html'
-  })],
+  plugins: [
+    partytownVite({
+      config: {
+        forward: ['dataLayer.push'],
+      },
+      dest: path.join(__dirname, 'dist', '~partytown'),
+    }),
+    visualizer({
+      filename: 'target/stats.html',
+    }),
+  ],
 };
