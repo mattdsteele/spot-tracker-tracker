8fc3bb3176baf0cde29f3af4bab18d46c60a788e
mattdsteele <orphum@gmail.com>
2023-01-15 20:50:16 -0600

    Fix merge conflict

--- frontend/spot-tracker-ui/src/main.ts
+++ frontend/spot-tracker-ui/src/main.ts
@@ -42,6 +42,7 @@ const state: Partial<{
   pings: Pings;
   course: course;
   transitions: GeofenceTransition[];
+}> = {};
 const zone = 'America/Chicago';
 
 // const omaha: LngLatLike  = [-95.98, 41.27695];
