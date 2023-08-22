6808ef084f0a9f2412fb6413ab73c90a06b4d407
mattdsteele <orphum@gmail.com>
2023-08-21 22:25:11 -0500

    Update debugging

--- frontend/spot-tracker-ui/src/push.ts
+++ frontend/spot-tracker-ui/src/push.ts
@@ -53,20 +53,15 @@ function urlBase64ToUint8Array(base64String: string) {
 }
 
 async function registerSubscription() {
-  alert('registering subscription');
   navigator.serviceWorker.register('service-worker.js');
-  alert('registered');
   const registration = await navigator.serviceWorker.ready;
-  alert('registered');
 
 
   // If we think it's Safari but doesn't have push manager, show specific instructions
   const ua = navigator.userAgent;
-  alert(ua);
   if (ua.includes('iPhone') || ua.includes('iPad')) {
-    alert('is ios')
-    if (!('pushManager' in registration)) {
-      alert('does not have push in registration')
+    if (!(registration.pushManager)) {
+      alert('determined does not have pushManager API')
       const bell = document.querySelector('.notifications');
       bell.classList.toggle('hidden');
       bell.addEventListener('click', () => {
@@ -77,27 +72,34 @@ async function registerSubscription() {
     }
   }
 
-  alert('getting push subscription');
-  const subscription = await registration.pushManager.getSubscription();
-  if (!subscription) {
-    const bell = document.querySelector('.notifications');
-    bell.classList.toggle('hidden');
-    bell.addEventListener('click', () => {
-      const dialog: SlDialog = document.querySelector('.notify-dialog');
-      dialog.show();
-      dialog.querySelector('sl-button').addEventListener('click', async () => {
-        await subscribe();
-        await dialog.hide();
-        bell.classList.toggle('hidden');
-      })
-    });
-  } else {
-    console.log('already have a subscription');
+  try {
 
-    console.log(
-      JSON.stringify({
-        subscription: subscription,
-      }),
-    );
+    const subscription = await registration.pushManager.getSubscription();
+    alert('got subscription');
+    alert(subscription);
+    if (!subscription) {
+      const bell = document.querySelector('.notifications');
+      bell.classList.toggle('hidden');
+      bell.addEventListener('click', () => {
+        const dialog: SlDialog = document.querySelector('.notify-dialog');
+        dialog.show();
+        dialog.querySelector('sl-button').addEventListener('click', async () => {
+          await subscribe();
+          await dialog.hide();
+          bell.classList.toggle('hidden');
+        })
+      });
+    } else {
+      console.log('already have a subscription');
+
+      console.log(
+        JSON.stringify({
+          subscription: subscription,
+        }),
+      );
+    }
+  } catch (e) {
+    alert('failed');
+    alert(e);
   }
 }
