2ea70604a61ab340b96cacacf111f347fb64ba92
Jessica C <jcake2@gmail.com>
2024-02-04 19:58:28 -0600

    Add stryker to pizza-function

---
+++ pizza-function/stryker.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+/** @type {import('@stryker-mutator/api/core').PartialStrykerOptions} */
+const config = {
+  _comment:
+    "This config was generated using 'stryker init'. Please take a look at: https://stryker-mutator.io/docs/stryker-js/configuration/ for more information.",
+  packageManager: "npm",
+  reporters: ["html", "clear-text"],
+  testRunner: "vitest",
+  testRunner_comment:
+    "Take a look at https://stryker-mutator.io/docs/stryker-js/vitest-runner for information about the vitest plugin.",
+  coverageAnalysis: "perTest",
+  buildCommand: "npm run build",
+};
+export default config;
