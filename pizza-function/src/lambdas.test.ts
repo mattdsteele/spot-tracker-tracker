be5711530913df56098227d5f0a1956f9184c575
Jessica C <jcake2@gmail.com>
2024-02-17 22:31:54 -0600

    Add another non-triggering scenario

--- pizza-function/src/lambdas.test.ts
+++ pizza-function/src/lambdas.test.ts
@@ -1,8 +1,21 @@
 import { test, expect } from "vitest";
 import { handler } from "./lambda";
 
-test('runs function', async () => {
+test('Does not trigger event when no data', async () => {
     const result = await handler({detail:{}}, {} as any, () => {});
+    expect(result.statusCode).toBe(200);
+    expect(result.body).toBe('{"message":"Did not trigger event"}');
+});
+
+test('Does not trigger event when not at Arlington', async () => {
+    const input = {
+        detail:{
+            EventType: 'EXIT',
+            GeofenceId: 'Somewhere Random',
+            SampleTime: '2024-02-17T22:23:24Z'
+        }
+    };
+    const result = await handler(input, {} as any, () => {});
     expect(result.statusCode).toBe(200);
     expect(result.body).toBe('{"message":"Did not trigger event"}');
 });
