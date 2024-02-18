e3dbaf8a43a1a7557ccf0b1ec5e05d0c94cc547c
Jessica C <jcake2@gmail.com>
2024-02-17 21:23:40 -0600

    Expectations matter!

--- pizza-function/src/lambdas.test.ts
+++ pizza-function/src/lambdas.test.ts
@@ -3,6 +3,6 @@ import { handler } from "./lambda";
 
 test('runs function', async () => {
     const result = await handler({detail:{}}, {} as any, () => {});
-    //expect(result.statusCode).toBe(200);
-    //expect(result.body).toBe('{"message":"Did not trigger event"}');
+    expect(result.statusCode).toBe(200);
+    expect(result.body).toBe('{"message":"Did not trigger event"}');
 });
