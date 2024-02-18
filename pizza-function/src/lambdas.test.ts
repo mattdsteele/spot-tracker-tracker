a22179c1ffb4d4c883c6778b21f3655e5da84d76
Jessica C <jcake2@gmail.com>
2024-02-17 22:33:39 -0600

    Add test that does send events
    Fix hanging test

--- pizza-function/src/lambdas.test.ts
+++ pizza-function/src/lambdas.test.ts
@@ -1,5 +1,13 @@
-import { test, expect } from "vitest";
+import { test, expect, afterEach, vi } from "vitest";
 import { handler } from "./lambda";
+import * as playwright from './caseys-playwright';
+import * as push from './push';
+import { SendResult } from "web-push";
+
+afterEach(() => {
+    vi.restoreAllMocks()
+    vi.unstubAllGlobals()
+})
 
 test('Does not trigger event when no data', async () => {
     const result = await handler({detail:{}}, {} as any, () => {});
@@ -18,4 +26,30 @@ test('Does not trigger event when not at Arlington', async () => {
     const result = await handler(input, {} as any, () => {});
     expect(result.statusCode).toBe(200);
     expect(result.body).toBe('{"message":"Did not trigger event"}');
+});
+
+test('Does trigger event for Arlington', async () => {
+    vi.stubGlobal('process', {env: {SPOT_PUSH_MATT_SUB: '{"endpoint":"hello"}'}});
+    let playwrightSpy = vi.spyOn(playwright, 'main');
+    playwrightSpy.mockImplementationOnce((_,__) => {
+        return Promise.resolve({
+            video: '',
+            simulation: true
+        })
+    })
+    
+    let pushSpy = vi.spyOn(push, 'sendPushEvent');
+    pushSpy.mockImplementation((_,__) =>{
+        return Promise.resolve({} as SendResult);
+    })
+
+    const input = {detail:{
+        EventType: 'EXIT',
+        GeofenceId: 'Arlington',
+        SampleTime: '2024-02-17T22:23:24Z'
+    }};
+    const result = await handler(input, {} as any, () => {});
+    expect(playwrightSpy).toHaveBeenCalledOnce();
+    expect(result.statusCode).toBe(200);
+    expect(JSON.parse(result.body)).toEqual(input);
 });
